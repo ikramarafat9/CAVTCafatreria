102f0d34d717b7ae9a41d0ff60bda48c2c08e276
ikramarafat9 <ikramarafat5119@gmail.com>
2025-07-30 14:55:05 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -993,5 +993,5 @@ def on_join(data):
 #---------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 443))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
